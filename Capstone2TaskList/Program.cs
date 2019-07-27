ef304f119fdd9d6505a886d8a5c52f1d207d1cad
Brandyn Tweedly <tweedly92@gmail.com>
2019-07-26 22:13:36 -0400

    First Commit

---
+++ Capstone2TaskList/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Capstone2TaskList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Task Manager!");
+            bool go = true;
+            while (go)
+            {
+                Console.WriteLine();
+                Task.DisplayMenu();
+                Console.WriteLine();
+                
+                int menuChoice = int.Parse(Console.ReadLine());
+
+                if (menuChoice == 1)
+                {
+                    int taskCount = 1;
+                    foreach (Task taskInfo in Task.taskList)
+                    {
+                        
+                        taskInfo.PrintTaskInfo(taskCount);
+                        taskCount++;
+                        Console.WriteLine();
+                    }
+                }
+                else if (menuChoice == 2)
+                {
+                    Console.WriteLine("Please enter new task info: ");
+
+                    Task.AddToList();
+                    
+                }
+                else if (menuChoice == 3)
+                {
+                    int taskCount2 = 1;
+                    foreach (Task taskInfo in Task.taskList)
+                    {
+                        taskInfo.PrintTaskInfo(taskCount2);
+                        taskCount2++;
+                        Console.WriteLine();
+                    }
+
+                    Console.WriteLine("What task number would you like to delete?");
+                    int delete = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Are you sure you want to delete task number " + delete + "? (y/n)");
+                    string deleteConfirm = Console.ReadLine();
+                    
+                        if (deleteConfirm == "y")
+                        {
+                            Task.taskList.RemoveAt(delete - 1);
+                            Console.WriteLine($"Task {delete} has been removed.");
+                        }
+                        else if (deleteConfirm == "n")
+                        {
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Please enter only a 'y' or 'n'. ");
+                            Console.WriteLine("You are being returned to the main menu.");
+                        }
+                    
+                }
+                else if (menuChoice == 4)
+                {
+                    Task.MarkChoice();
+                }
+                else if(menuChoice == 5)
+                {
+                    Console.WriteLine("Have a great day");
+                }
+                else
+                {
+
+                }
+
+            }
+        }
+    }
+}
---
+++ Capstone2TaskList/Task.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone2TaskList
+{
+    class Task
+    {
+        #region Lists
+        public static List<Task> taskList = new List<Task>
+            {
+                new Task("Brandyn", "Eat ass", "07/30/2019", false),
+                new Task("Josh", "Mow the lawn", "07/30/2019", false),
+                new Task("Joe", "Brandyn is smrt", "07/30/2019", false)
+
+            };
+        
+        #endregion
+        #region Fields
+
+        private string TMName;
+        private string TaskDescription;
+        private string DueDate;
+        private bool Completed;
+
+        #endregion
+
+        #region Constructors
+
+        public Task()
+        {
+            TMName = "Please enter valid information.";
+            TaskDescription = "Please enter valid information.";
+            DueDate = "Pleae enter valid information";
+            Completed = false;
+        }
+        public Task(string _tmName, string _taskDescription, string _dueDate, bool _completed)
+        {
+            TMName = _tmName;
+            TaskDescription = _taskDescription;
+            DueDate = _dueDate;
+            Completed = _completed;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public bool IsCompleted(string completed)
+        {
+            if(completed == "yes")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static void DisplayMenu()
+        {
+            Console.WriteLine("==================================");
+            Console.WriteLine("\t1. List tasks");
+            Console.WriteLine("\t2. Add task");
+            Console.WriteLine("\t3. Delete task");
+            Console.WriteLine("\t4. Mark task complete");
+            Console.WriteLine("\t5. Quit");
+            Console.WriteLine("==================================");
+            Console.WriteLine("What would you like to do?");
+        }
+
+        public void PrintTaskInfo(int count)
+        {
+            Console.WriteLine(count + ". Team Member Name: " + TMName);
+            Console.WriteLine("\tTask Description: " + TaskDescription);
+            Console.WriteLine("\tDue Date: " + DueDate);
+            Console.WriteLine("\tComepleted: " + Completed);
+        }
+
+        public static void AddToList()
+        {
+            Task taskList1 = new Task();
+
+            Console.WriteLine("Please enter a name: ");
+            taskList1.TMName = Console.ReadLine();
+
+            Console.WriteLine("Please enter a task description: ");
+            taskList1.TaskDescription = Console.ReadLine();
+
+            Console.WriteLine("Please enter a due date: ");
+            taskList1.DueDate = Console.ReadLine();
+
+            Console.WriteLine("Is the task completed yet (y/n): ");
+            string isCompleted = Console.ReadLine().ToLower();
+            
+                if (isCompleted == "y")
+                {
+                    taskList1.Completed = true;
+                }
+                else if (isCompleted == "n")
+                {
+                    taskList1.Completed = false;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid response. (y/n)");
+                    
+                }
+            
+            taskList.Add(taskList1);
+
+        }
+        public static void MarkChoice()
+        {
+            Console.WriteLine("Which task would you like to mark as complete?");
+            Console.WriteLine();
+
+            int taskCount = 1;
+            foreach (Task taskInfo in Task.taskList)
+            {
+                taskInfo.PrintTaskInfo(taskCount);
+                taskCount++;
+                Console.WriteLine();
+            }
+
+
+            int userInput = int.Parse(Console.ReadLine());
+            userInput--;
+
+            Console.WriteLine("Are you sure you want to change this?(y/n)");
+            string answer = Console.ReadLine();
+            if (answer == "y")
+            {
+                taskList[userInput].Completed = true;
+            }
+            else
+            {
+
+            }
+        }
+        
+
+        
+
+
+        #endregion
+    }
+}
